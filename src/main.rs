f271b6123c6ad1193bb638d92c1aea47458a0a92
frchandra <nismara.chandra@gmail.com>
2026-01-16 22:29:06 +0900

    feat: hello world http

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+use axum::{serve, Router};
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    serve(listener, app).await.unwrap();
 }
